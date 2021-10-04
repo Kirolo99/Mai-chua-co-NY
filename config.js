781b39a511af97b63a1474554ccbe5a99b96e456
Khánh <91839948+Kirolo99@users.noreply.github.com>
2021-10-04 12:52:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    introDesc: `Em có điều này muốn hỏi Anh, Anh nhớ phải trả lời thật lòng nha`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    title: 'Em biết Anh thích Em phải không phải không ? 😙',
+    desc: 'Anh không trả lời hoặc thoát ra, tức là Anh thích Em đó nha :> ',
+    btnYes: 'Có, Anh thật sự thích Em 💖💖💖',
+    btnNo: '💔 Em mơ đi cưng ! 💔 ',
+    question:'Nói cho Em biết đi vì sao Anh thích Em 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    reply: 'Tại vì Mai xinh gái và cute đó Ahiiiii 😊😊',
+    mess: 'Em biết mà, Em cũng thích Anh lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Em nha. 💬💬',
     btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    messLink: 'https://m.me/maii.ngocc.944'
 }
